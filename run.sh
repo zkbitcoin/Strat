264219f4410312e6090526a7b6360cc44b414a07
zkbitcoin <github@zkbitcoin.com>
2024-10-14 20:19:23 -0400

    update

--- run.sh
+++ run.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
-{
 
+{
   if lsof -i :8003; then
     echo "A process is still using the port. Killing it..."
     kill -9 $(lsof -t -i:8003)
   fi
 
-  #cabal exec strat-exe -- -d4 -rdebug03
-  stack exec ${HOME}/go/src/blockbook/games/ai/server/Strat/dist-newstyle/build/aarch64-osx/ghc-9.4.8/Strat-0.1.0.0/x/strat-exe/build/strat-exe/strat-exe -- -n checkersWeb
-#} 2>&1 | tee output.file
+  # Determine the OS and set the command accordingly
+  if [ "$(uname)" = "Darwin" ]; then
+    # macOS
+    stack exec ${HOME}/go/src/blockbook/games/ai/server/Strat/dist-newstyle/build/aarch64-osx/ghc-9.4.8/Strat-0.1.0.0/x/strat-exe/build/strat-exe/strat-exe -- -n checkersWeb
+  elif [ "$(uname)" = "Linux" ]; then
+    # Linux
+    stack exec ${HOME}/go/src/blockbook/games/ai/server/Strat/dist-newstyle/build/x86_64-linux/ghc-9.4.8/Strat-0.1.0.0/x/strat-exe/build/strat-exe/strat-exe -- -n checkersWeb
+  else
+    echo "Unsupported OS"
+    exit 1
+  fi
 } > /dev/null 2>&1
+
